85aec71d9e28b6e7274c2c2ca6c4e81df1831886
Svitlana Zaster <szaster@protonmail.com>
2021-04-16 17:29:54 -0500

    installed material-ui

--- src/App.js
+++ src/App.js
@@ -2,20 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1 ">
+      <h1 className="f2-l">Bolus timer</h1>
+      <input placeholder="type your BG" />
+
+      <button className> Run timer </button>
     </div>
   );
 }
