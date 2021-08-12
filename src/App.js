6ba9b18c37e7f9f96bb3f2160d98d6d3e5187714
Svitlana Zaster <szaster@protonmail.com>
2021-08-11 23:13:21 -0500

    some .css fiel edits

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1 ">
+    <div className="App">
       <h1 className="f2-l">Bolus timer</h1>
       <input placeholder="type your BG" />
 
